7b1906ea5a13072f5fe5eb70f15d3ee1f5fdaa4a
Ashmita <ashmitarathod11@gmail.com>
2026-08-20 00:11:54 +0530

    Solve Contains Duplicate

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+
+            if (set.contains(num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        ContainsDuplicate obj = new ContainsDuplicate();
+
+        int[] nums = {1, 2, 3, 1};
+
+        boolean result = obj.containsDuplicate(nums);
+
+        System.out.println("Contains Duplicate: " + result);
+    }
+}
