6574071c82c9ac4b47d5f2a0818391f4d1f9fcce
Ashmita <ashmitarathod11@gmail.com>
2026-09-24 22:23:41 +0530

    Add Leetcode 232 Implete Queue using Stacks

---
+++ Stack and Queue/MyQueue.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+class MyQueue {
+
+    Stack<Integer> inStack;
+    Stack<Integer> outStack;
+
+    public MyQueue() {
+        inStack = new Stack<>();
+        outStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inStack.push(x);
+    }
+
+    public int pop() {
+        moveIfNeeded();
+        return outStack.pop();
+    }
+
+    public int peek() {
+        moveIfNeeded();
+        return outStack.peek();
+    }
+
+    public boolean empty() {
+        return inStack.isEmpty() && outStack.isEmpty();
+    }
+
+    private void moveIfNeeded() {
+        if (outStack.isEmpty()) {
+            while (!inStack.isEmpty()) {
+                outStack.push(inStack.pop());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        MyQueue queue = new MyQueue();
+
+        queue.push(10);
+        queue.push(20);
+        queue.push(30);
+
+        System.out.println("Peek: " + queue.peek());
+        System.out.println("Pop: " + queue.pop());
+        System.out.println("Peek after pop: " + queue.peek());
+        System.out.println("Is Empty: " + queue.empty());
+    }
+}
